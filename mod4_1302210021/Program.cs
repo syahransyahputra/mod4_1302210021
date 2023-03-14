4b1a2dfc1fbd50961ac28aa4d87bb1e49d191d42
PC32-IFLAB <127027989+syahransyahputra@users.noreply.github.com>
2023-03-14 16:36:16 +0700

    menambahkan class kode buah

--- mod4_1302210021/Program.cs
+++ mod4_1302210021/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace mod4_1302210021
+﻿public class kodeBuah
 {
-    internal class Program
+
+    public enum buah { apel, aprikot, alpukat, pisang, paprika, blackberry, ceri, kelapa, jagung };
+
+    public static string getKodeBuah(buah BUAH)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hellosdfsdasdfdfskl!");
-        }
+        string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+        return kodeBuah[(int)BUAH];
     }
 }
