abfbcab06a6b290fb28466352af243357a704884
PC32-IFLAB <PC32-IFLAB>
2023-03-14 15:53:14 +0700

    first commit

---
+++ mod4_1302210021/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mod4_1302210021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
